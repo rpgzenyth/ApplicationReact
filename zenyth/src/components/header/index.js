5779f22f446e7b9fe534210aecf0fd8538425cab
eleacarton <eleacarton@me.com>
2021-11-24 11:50:46 +0100

    History back

--- zenyth/src/components/header/index.js
+++ zenyth/src/components/header/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {StyledHeader,Titre} from '../../config/styled'
 import {MdOutlineKeyboardArrowLeft} from 'react-icons/md'
+import { useHistory } from 'react-router';
 
 const Header = () => {
+
+    const history = useHistory()
+
     return (
       <div>
         <StyledHeader>
-          <MdOutlineKeyboardArrowLeft size="30px" style= { {position: "absolute", left: "1em", top: "50%", transform: "translateY(-50%)", color: 'white' } }/>
-          <Titre>Zenyth rpg</Titre>
+          <MdOutlineKeyboardArrowLeft onClick={ () => history.goBack() } size="30px" style= { {position: "absolute", left: "1em", top: "50%", transform: "translateY(-50%)", color: 'white' } }/>
+          <Titre onClick={ () => history.push(`/`) }>Zenyth rpg</Titre>
         </StyledHeader>
       </div>
     )
