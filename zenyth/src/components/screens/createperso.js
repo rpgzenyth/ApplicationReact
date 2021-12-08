9acde913f84071c318ba2a7bd8e9f77bbe0e64f1
Makory13 <ysmakory13@gmail.com>
2021-12-08 10:21:32 +0100

    form create perso

--- zenyth/src/components/screens/createperso.js
+++ zenyth/src/components/screens/createperso.js
@@ -1,11 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FormCreatePerso, InputStyled, Link } from '../../config/styled';
 
-const Parameters = () => {
+const Createperso = () => {
+    
+    const [CaracterName, setCaracterName] = useState('')
+    const [CaracterClasse, setCaracterClass] = useState('')
+    const [CaracterRace, setCaracterRace] = useState('')
+    const [CaracterExp, setCaracterExp] = useState('')
+    const [CaracterClasse, setCaracterClass] = useState('')
+    const [CaracterClasse, setCaracterClass] = useState('')
+    const [CaracterClasse, setCaracterClass] = useState('')
+
     return (
-        <div>
-            <h1>Parameters</h1>
+        <FormCreatePerso onSubmit={onsubmit}>
+
+        <div className='subtitleForm'>
+            <h1>Nom du personnage</h1>
         </div>
-    );
+
+        <InputStyled
+            value={CaracterName}
+            onChange={e => setCaracterName(e.target.value)}
+            name='CaracterName'
+            placeholder='Nom du personnage'
+        ></InputStyled>
+        <InputStyled
+            value={CaracterRace}
+            onChange={e => setCaracterRace(e.target.value)}
+            name='CaracterRace'
+            placeholder='Race'
+        ></InputStyled>
+        <InputStyled
+            value={CaracterClasse}
+            onChange={e => setCaracterClass(e.target.value)}
+            name='CaracterClass'
+            placeholder='Classe'
+        ></InputStyled>
+
+        <InputStyled
+            value={CaracterExp}
+            onChange={e => setCaracterExp(e.target.value)}
+            name='CaracterExp'
+            placeholder='Expericence'
+        ></InputStyled>
+        <InputStyled
+            value={CaracterClasse}
+            onChange={e => setCaracterClass(e.target.value)}
+            name='CaracterClass'
+            placeholder='Classe'
+        ></InputStyled>
+        <InputStyled
+            value={CaracterClasse}
+            onChange={e => setCaracterClass(e.target.value)}
+            name='CaracterClass'
+            placeholder='Classe'
+        ></InputStyled>
+        <Link type='submit' placeholder='Connexion'></Link>
+        </FormCreatePerso>
+    )
 };
 
-export default Parameters;
+export default Createperso;
