05bf5cb682ba63e6799aaab4f3b50babe39852f2
VENOCKA <thomas.ruiz@ynov.com>
2022-11-25 09:46:46 +0100

    Remove log

--- zenyth/src/hooks/usePlayerData.js
+++ zenyth/src/hooks/usePlayerData.js
@@ -3,7 +3,6 @@ import { post } from "../utils/api";
 export const login = (token) => {
     const resp = post(`${process.env.REACT_APP_API_URL}/login`, token).then(
         (response) => {
-            console.log(response)
             if (response.status !== 200) {
                 console.warn("Request failed. Status code: " + response.status);
                 return;
