c36658c68de2a18bb062c80d5b749258e8f9d15b
CROC <clement.roc@ynov.com>
2022-12-07 14:01:40 +0100

    register

---
+++ zenyth/src/components/screens/register.js
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import {
+  Btn,
+  LabelForm,
+  TitleForm,
+  TitleLegend,
+  InputStyled,
+  ContentDiv,
+} from "../style/exportedStyle";
+import { register } from "../../hooks/usePlayerData";
+import { setToken } from "../../utils/token";
+
+const Register = () => {
+  const [player, setPlayer] = useState();
+  const [errorMessage, setErrorMessage] = useState();
+
+  const history = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    register(player)
+      .then((data) => {
+        if(!data){
+            setErrorMessage("Une erreur est survenue lors de la création du compte !")
+        }else {
+            setToken(data.token);
+            history("/menu");
+        }
+      })
+      .catch((error) => {
+        console.log(error)
+        setErrorMessage(error.response.data.message);
+      });
+  };
+
+  return (
+    <ContentDiv>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <TitleForm>Créer un compte</TitleForm>
+        <div>
+          <LabelForm>FirstName</LabelForm>
+          <InputStyled
+            onChange={(e) => {
+              setPlayer({ ...player, firstName: e.target.value });
+            }}
+            name="firstname"
+          ></InputStyled>
+        </div>
+
+        <div>
+          <LabelForm>LastName</LabelForm>
+          <InputStyled
+            onChange={(e) => {
+              setPlayer({ ...player, lastName: e.target.value });
+            }}
+            name="lastname"
+          ></InputStyled>
+        </div>
+
+        <div>
+          <LabelForm>Adresse mail</LabelForm>
+          <InputStyled
+            onChange={(e) => {
+              setPlayer({ ...player, email: e.target.value });
+            }}
+            name="email"
+          ></InputStyled>
+        </div>
+
+        <div>
+          <LabelForm>Mot de passe</LabelForm>
+          <InputStyled
+            onChange={(e) => {
+              setPlayer({ ...player, password: e.target.value });
+            }}
+            name="password"
+            type="password"
+          ></InputStyled>
+        </div>
+
+        {!errorMessage ? (
+          ""
+        ) : (
+          <div>
+            <TitleLegend>{errorMessage}</TitleLegend>
+          </div>
+        )}
+
+        <Btn type="submit">
+          Créer
+        </Btn>
+      </form>
+    </ContentDiv>
+  );
+};
+
+export default Register;
